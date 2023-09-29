5bf25e33efa93be1d7a09a76ecc161c5f8dc842f
Snowflakegh <146144182+Snowflakegh@users.noreply.github.com>
2023-09-29 01:02:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,39 +28,28 @@ def get_fruityvice_data(this_fruit_choice):
     return streamlit.dataframe(fruityvice_normalized)
 
 # New Section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
-        streamlit.error("Please select a fruit to get information.")
-    else:
-        back_from_function = get_fruityvice_data(fruit_choice)
-        streamlist.dataframe(back_from_function)
-
-except URLError as e:
-streamlit.error()
-
-streamlit.stop()
+#streamlit.error()
+#streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
     with my_cur = my_cnx.cursor() as my_cur:
-        my_cur.execute("SELECT * from fruit_load_list")
+        my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
 
 if streamlit.button('Get Fruit Load List'):
-    my_cnx=snowflake.connector.connect(**streamlit.screts["snowflake"])
-    my_data_rows=get_fruit_load_list()
+    my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
 def insert_row_snowflake(new_fruit):
     with my_cur = my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_laod_list values ('from streamlist')")
+        my_cur.execute("insert into fruit_laod_list values ('from streamlit')")
         return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
-    my_cnx=snowflake.connector.connect(**streamlit.screts["snowflake"])
+    my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlist.dataframe(back_from_function)
  
